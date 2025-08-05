6523c94b03fa0ee2e9b88a458b1773747afa07d4
Seniru Senarathne <senirusenarathne@gmail.com>
2025-08-05 22:05:47 +0530

    Add WebSocket support for real-time notifications in PopupNotification

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,8 @@
 const config = {
     // API endpoints
     endpoints: {
-        liveCount: "https://script.google.com/macros/s/AKfycbylf8kXqM8zoVASg20ng6_jmRVrDupvi1GB73BeBoTf6ehgvfL1Lj4ieARQny6WywoW6g/exec"
+        liveCount: "https://script.google.com/macros/s/AKfycbylf8kXqM8zoVASg20ng6_jmRVrDupvi1GB73BeBoTf6ehgvfL1Lj4ieARQny6WywoW6g/exec",
+        websocket: process.env.REACT_APP_WEBSOCKET_URL || 'wss://sme-api-04db435264b2.herokuapp.com:3002'
     },
 
     // Refresh intervals in milliseconds
