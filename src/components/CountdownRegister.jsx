3c7579a397bbbb9fd2d3fdb8085c86173c72af2b
Vidul <your_email@example.comvidulsathmika@gmail.com>
2026-05-10 22:54:49 +0530

    changed the wording of the login button

--- src/components/CountdownRegister.jsx
+++ src/components/CountdownRegister.jsx
@@ -31,7 +31,7 @@ const CountdownRegister = () => {
             <div className="register-actions">
               <RegisterNow className="small-button" />
               <a href="/mysme/login" className="register-button small-button login-button">
-                Login to My SME
+                My SME Login
               </a>
             </div>
 
